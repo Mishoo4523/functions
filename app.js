b17404c23bde6422eb8042d5bba2c6513bfeb0ac
Mishoo4523 <mishomurad@gmail.com>
2022-07-29 15:55:18 +0300

    functions

---
+++ app.js
@@ -0,0 +1,6 @@
+function calcAge(age) {
+   
+    let result = age*365 ;
+    console.log(result);
+}
+calcAge(34);
---
+++ pro.js
@@ -0,0 +1,15 @@
+function calc(...numbers){
+
+    let results = 0;
+
+    for ( let i=0; i<numbers.length ; i++);{
+    result += numbers[i];
+
+
+}
+console.log(result);
+}
+
+calc();
+
+
